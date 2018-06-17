9b347633e0cdf709d4b2ad97af3168483c6a48f5
nightstorm0909 <nilotpalsinha1990@gmail.com>
2018-06-17 22:30:57 +0800

    3

--- visualize_architecture.py
+++ visualize_architecture.py
@@ -6,3 +6,4 @@ from keras.utils import plot_model
 
 model = LeNet.build(28, 28, 1, 10)
 plot_model(model, to_file = "lenet.png", show_shapes = True)
+# plot_model(model, to_file='model.png', show_shapes=False, show_layer_names=True, rankdir='TB')
